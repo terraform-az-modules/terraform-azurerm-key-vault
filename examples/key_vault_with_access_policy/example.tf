4a33989d71e8a25f20cf5c1fc7475ecf75d95652
Vedant <vedant.kalia@clouddrove.com>
2025-10-14 01:25:52 +0530

    fix: Updated Module Version

--- examples/key_vault_with_access_policy/example.tf
+++ examples/key_vault_with_access_policy/example.tf
@@ -9,7 +9,7 @@ data "azurerm_client_config" "current_client_config" {}
 # ------------------------------------------------------------------------------
 module "resource_group" {
   source      = "terraform-az-modules/resource-group/azure"
-  version     = "1.0.0"
+  version     = "1.0.1"
   name        = "core"
   environment = "dev"
   location    = "centralus"
@@ -21,7 +21,7 @@ module "resource_group" {
 # ------------------------------------------------------------------------------
 module "vnet" {
   source              = "terraform-az-modules/vnet/azure"
-  version             = "1.0.0"
+  version             = "1.0.1"
   name                = "core"
   environment         = "dev"
   label_order         = ["name", "environment", "location"]
